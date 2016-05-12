4b6f1166e359cc0fc982d12ae90ab30d2ab49cb9
Trieu Nguyen <trieu.nguyennhu@gmail.com>
2016-05-12 17:32:20 +0700

    Bat dau trang login admin

--- protected/app/Modules/Backend/routes.php
+++ protected/app/Modules/Backend/routes.php
@@ -5,5 +5,9 @@ Route::group(['module'=>'Admin'], function(){
     Route::get('/admin/', function(){
         return view('backend.index');
     });
+	
+	Route::get('/admin/login', function(){
+        return view('backend.login');
+    });
     
 });
